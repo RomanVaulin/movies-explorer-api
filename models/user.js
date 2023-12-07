1ab3f18c58cac3e92cebc3db21cceb9e28542b0a
Roman Vaulin <vaulin007@yandex.ru>
2023-12-07 13:33:08 +0500

    diplom level-1 1.0

---
+++ models/user.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+// const validator = require('validator');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+// const urlRegex = require('../utils/constants');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Поле "name" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля - 2'],
+    maxlength: [30, 'Максимальная длина поля - 30'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Поле "email" должно быть заполнено'],
+    unique: true,
+    validate: {
+      validator(email) {
+        return /^\S+@\S+\.\S+$/.test(email);
+      },
+      message: 'Введите корректный email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Поле "password" должно быть заполнено'],
+    select: false,
+  },
+}, { versionKey: false });
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError('Неправильные почта или пароль');
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new UnauthorizedError('Неправильные почта или пароль');
+          }
+
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
